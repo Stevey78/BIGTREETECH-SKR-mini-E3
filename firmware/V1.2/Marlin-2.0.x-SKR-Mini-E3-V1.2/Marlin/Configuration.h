3b75d6f7801dd215c9d5bc791a9ea593c776b4f3
Stevey78 <48095364+Stevey78@users.noreply.github.com>
2020-07-12 23:08:01 -0400

    Update Configuration.h

--- firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
+++ firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
@@ -946,7 +946,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -40, -10, -1.7 }
+#define NOZZLE_TO_PROBE_OFFSET { -40, -9, -1.1 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
